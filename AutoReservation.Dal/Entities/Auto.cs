251f0c379d7b6ad959068ad0e20c5fda5e4e688c
andopen <andre.bachmann89@gmx.ch>
2017-11-22 14:35:49 +0100

    Datenbankklassen Auto, Kunde und Reservation erstellt

--- AutoReservation.Dal/Entities/Auto.cs
+++ AutoReservation.Dal/Entities/Auto.cs
@@ -1,8 +1,24 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 
+
 namespace AutoReservation.Dal.Entities
 {
- 
+    public class Auto
+    {
+        [Key]
+        int Id { get; set; }
+        string Marke { get; set; }
+        byte RowVersion { get; set; }
+        int Tagestarif { get; set; }
+        [ForeignKey("Reservationen")]
+        ICollection<Reservation> Reservationen{get; set;}
+
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+
+        //AutoKlassen fehlen noch
+    }
 }
--- AutoReservation.Dal/Entities/Reservation.cs
+++ AutoReservation.Dal/Entities/Reservation.cs
@@ -4,5 +4,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoReservation.Dal.Entities
 {
+    public class Reservation
+    {
+        [Key]
+        int AutoId { get; set; }
+        DateTime Bis { get; set; }
+        [ForeignKey("KundenId")]
+        int KundenId { get; set; }
+        int ReservationsNr { get; set; }
+        byte RowVersion { get; set; }
+        DateTime Von { get; set; }
+
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+    }
     
 }
