3c5b4593bc5d847b488e6761784d3bf522a8443f
feybro <frossett@hsr.ch>
2017-12-13 15:30:29 +0100

    [MGE] Added the save command to the client

--- AutoReservation.GUI/ViewModels/ClientViewModel.cs
+++ AutoReservation.GUI/ViewModels/ClientViewModel.cs
@@ -3,8 +3,10 @@ using AutoReservation.GUI.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace AutoReservation.GUI.ViewModels
@@ -34,5 +36,36 @@ namespace AutoReservation.GUI.ViewModels
         }
 
         private bool CanNew() => ServiceExists;
+
+        private RelayCommand saveCommand;
+
+        public ICommand SaveCommand => saveCommand ?? (saveCommand = new RelayCommand(param => SaveData(), () => CanSaveData()));
+
+        private void SaveData()
+        {
+            try
+            {
+                foreach (var client in items)
+                {
+                    if (client.Id == default(int))
+                    {
+                        Service.InsertKunde(client);
+                        Load();
+                    }
+                    else
+                    {
+                        Service.UpdateKunde(client);
+                    }
+                }
+                Load();
+            }
+            catch (FaultException e)
+            {
+                MessageBox.Show(e.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private bool CanSaveData() => ServiceExists;
+
     }
 }
